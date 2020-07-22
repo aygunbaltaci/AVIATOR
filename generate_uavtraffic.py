299c81b58b2f14b5c6d50517d07620a9e8ff5ad6
Aygün Baltaci <aygunbaltaci@gmail.com>
2020-07-22 18:06:08 +0200

    Bugs are found that slow down the packet generation cycles. DL data traffic model is now aligned with actual model from Spark

--- generate_uavtraffic.py
+++ generate_uavtraffic.py
@@ -36,7 +36,7 @@ label_x = ['Data Rate (kbps)', 'Packet Inter-arrival (ms)', 'Packet Length (byte
 label_y = 'Density'
 legend = 'Simulated data'
 legend_location = (0.5, 0.05)
-numofbins = [15, 10, 10] # recommended bin sizes for data rate, packet interval, pkt length, respectively 10], for ul: []
+numofbins = [15, 20, 10] # recommended bin sizes for data rate, packet interval, pkt length, respectively 10], for ul: []
 
 # outputfiles-related
 outputfile_packets_extension = 'pcap'
@@ -58,17 +58,18 @@ port_destination = 47814
 # ========================================
 frequency_buffer = 3
 # downlink
-frequency_land_takeoff = 7
-frequency_pitch_roll = 2
-frequency_throttle_yaw = 1
-frequency_return_home = 5
+frequency_land_takeoff = 7 # 7
+frequency_pitch_roll = 3 # 2
+frequency_throttle_yaw = 5 # 1
+frequency_return_home = 13 # 5
+frequency_test = 13
 # uplink
 frequency_batterystatus_camerastatus = 6
 frequency_imustatus_rotorstatus = 2
 frequency_video = 1
 
 # ======== Send downlink data to UDP buffer
-def data_to_buffer_downlink(buffer, i, land_takeoff, pitch_roll, return_home, throttle_yaw):
+def data_to_buffer_downlink(buffer, i, land_takeoff, pitch_roll, return_home, throttle_yaw, test):
 	# add data to UDP buffer
 	if i % frequency_throttle_yaw == 0:
 		buffer += (throttle_yaw)
@@ -78,6 +79,8 @@ def data_to_buffer_downlink(buffer, i, land_takeoff, pitch_roll, return_home, th
 		buffer += (land_takeoff)
 	if i % frequency_return_home == 0:
 		buffer += (return_home)
+	#if i % frequency_test == 0:
+	#	buffer += (test)
 	return buffer
 
 # ======== Send uplink data to UDP buffer
@@ -96,11 +99,12 @@ def data_to_buffer_uplink(batterystatus, buffer, camerastatus,
 
 # ======== Generate data for downlink channel
 def generate_data_downlink():
+	test = 'x' * np.random.choice([2**5 + 2**4, 2**6 + 2**4])
 	land_takeoff = 't' * np.random.choice([2**5, 2**6, 2**7])
-	pitch_roll = 'r' * np.random.choice([2**5, 2**6, 2**7])
-	return_home = 'h' * np.random.choice([2**5, 2**6, 2**7])
-	throttle_yaw =  'l' * np.random.choice([2**5, 2**6, 2**7])
-	return land_takeoff, pitch_roll, return_home, throttle_yaw
+	pitch_roll = 'r' * np.random.choice([2**5 + 2**4, 2**6])
+	return_home = 'h' * np.random.choice([2**5 + 2**4, 2**6 + 2**4])
+	throttle_yaw =  'l' * np.random.choice([2**5, 2**5 + 2**4])
+	return land_takeoff, pitch_roll, return_home, throttle_yaw, test
 
 # ======== Generate data for uplink channel
 def generate_data_uplink():
@@ -119,6 +123,7 @@ def graph_generate(datarate, downlink, filename_extension, pkt_interarrival, pkt
 	cnt = 0
 	fig, host = prepare_graph()
 	datarate = list(filter(None, datarate)) # remove empty entries
+	#print(datarate)
 	for i in [datarate, pkt_interarrival, pkt_length]:
 		host[0, cnt] = histogram(numofbins[cnt], i, label_x[cnt], label_y, host[0, cnt]) # generate hist graphs
 		cnt += 1
@@ -144,9 +149,9 @@ def histogram(bins, data, label_x, label_y, plot):
 # ======== Application layer - Generate data based on the applications
 def layer_application(buffer, downlink, i, uplink):
 	if downlink:
-		land_takeoff, pitch_roll, return_home, throttle_yaw = generate_data_downlink() # fetch data
+		land_takeoff, pitch_roll, return_home, throttle_yaw, test = generate_data_downlink() # fetch data
 		buffer = data_to_buffer_downlink(buffer, i, land_takeoff, pitch_roll, # send data to buffer
-				return_home, throttle_yaw)
+				return_home, throttle_yaw, test)
 	else:
 		batterystatus, camerastatus, imustatus, rotorstatus, video = generate_data_uplink() # fetch data
 		buffer = data_to_buffer_uplink(batterystatus, buffer, camerastatus, i, # send data to buffer
@@ -157,50 +162,71 @@ def layer_application(buffer, downlink, i, uplink):
 def layer_transport(buffer, datarate, downlink, firstrun, i, num_packets,
 		pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous, uplink):
 	if i % frequency_buffer == 0:
-			first_loop = True
-			j, k = 0, 0
-			buffer_length = len(buffer)
+		first_loop = True
+		j, k = 0, 0
+		buffer_length = len(buffer)
+		sleep_dl = np.random.exponential(0.2) * 0.01 + 0.015
+		#print(np.random.exponential(0.5))
+		time.sleep(sleep_dl)
+		while True:
+			if (downlink and len(buffer) == 0) or (uplink and (j == math.ceil(buffer_length / pkt_length_maximum))): # buffer is emptied, exit the loop
+				break
+			delayProb = np.random.uniform(0, 1)
+			
+			#if buffer[len(buffer) - 1 - k] == 'l' and buffer[len(buffer) - 2 - k] != 'l': # generate packets based on one parameter - old method
+			if (downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k])) or uplink: # generate packets per parameter - new method
+				if not first_loop and ((downlink and delayProb > 0.95) or (uplink and delayProb > 0.8)): # probability for processing delay. Probability for dl and ul different to make the 2nd peak obvious on DL
+					#time_sleep = np.random.uniform(0, frequency_buffer / 10) # generate processing delay
+					time_sleep = np.random.uniform(0, frequency_buffer / 150) # generate processing delay
+					#time_sleep = np.random.exponential(0.2) * frequency_buffer / 250 
+					time.sleep(time_sleep)
 
-			while True:
-				if (downlink and (k == len(buffer) or len(buffer) == 0)) or (uplink and (j == math.ceil(buffer_length / pkt_length_maximum))): # buffer is emptied, exit the loop
-					break
-				delayProb = np.random.uniform(0, 1)
-				
-				#if buffer[len(buffer) - 1 - k] == 'l' and buffer[len(buffer) - 2 - k] != 'l': # generate packets based on one parameter - old method
-				if (downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k])) or uplink: # generate packets per parameter - new method
-					if not first_loop and ((downlink and delayProb > 0.95) or (uplink and delayProb > 0.8)): # probability for processing delay. Probability for dl and ul different to make the 2nd peak obvious on DL
-						time_sleep = np.random.uniform(0, frequency_buffer / 10) # generate processing delay
-						time.sleep(time_sleep)
-
-				#if downlink and buffer[len(buffer) - 1 - k] == 'l' and buffer[len(buffer) - 2 - k] != 'l': # generate packets based on one parameter - old method
-				if downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k]): # generate downlink packets
-					pkt = pkt_create(buffer[len(buffer) - 1 - k:]) # generate packet
-					buffer = buffer[:len(buffer) - 1 - k] # remove packet from buffer
-					datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = statistics_results(datarate, # generate stats
-							firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
-					k = 0
-				elif downlink: 
-					k += 1
-				elif uplink: # generate uplink packets
+			#if downlink and buffer[len(buffer) - 1 - k] == 'l' and buffer[len(buffer) - 2 - k] != 'l': # generate packets based on one parameter - old method
+			if downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k]): # generate downlink packets
+				pkt = pkt_create(buffer[len(buffer) - 1 - k:]) # generate packet
+				buffer = buffer[:len(buffer) - 1 - k] # remove packet from buffer
+				datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = statistics_results(datarate, # generate stats
+						firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
+				k = 0
+			elif downlink and k == len(buffer): 
+				#print("Last pkt buffer: %s" %buffer)
+				#print("Last pkt len: %d" %len(buffer))
+				pkt = pkt_create(buffer) # generate packet
+				buffer = '' # remove packet from buffer
+				datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = statistics_results(datarate, # generate stats
+						firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
+				k = 0
+			elif downlink: 
+				k += 1
+			elif uplink: # generate uplink packets
+				if j == math.ceil(buffer_length / pkt_length_maximum) - 1: # last pkt
+				 	#print("Last pkt len: %d" %len(buffer))
+				 	pkt = pkt_create(buffer) # generate packet
+				 	buffer = '' # buffer[:len(buffer) - 1 - pkt_length_maximum] # remove packet from buffer
+				 	datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = statistics_results(datarate, # generate stats
+				 			firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
+				else: 
 					pkt = pkt_create(buffer[len(buffer) - 1 - pkt_length_maximum:]) # generate packet
 					buffer = buffer[:len(buffer) - 1 - pkt_length_maximum] # remove packet from buffer
 					datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = statistics_results(datarate, # generate stats
 							firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
-					j += 1
-				first_loop = False
-			sys.stdout.write("Number of generated packets = %d out of %d   \r" %(len(pkt_interarrival), num_packets))
-			sys.stdout.flush()
+				j += 1
+
+			first_loop = False	
+		sys.stdout.write("Number of generated packets = %d out of %d   \r" %(len(pkt_interarrival), num_packets))
+		sys.stdout.flush()
 	return buffer, datarate, firstrun, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous
 
 # ======== Main function
 def main():
 	buffer = ''
 	pkt_list, pkt_interarrival, pkt_length, datarate = [], [], [], []
-	i, time_previous, pkt_length_total = 0, 0, 0
+	i, time_previous, pkt_length_total, j = 0, 0, 0, 0
 	args, filename_extension, title = parse_args()
 	firstrun = True
 	
 	print("\nPacket generation begins on %s channel" %title)
+	starttime = time.time()
 	# main loop
 	while True:
 		buffer = layer_application(buffer, args.downlink, i, args.uplink) # run app layer
@@ -208,12 +234,17 @@ def main():
 				buffer, datarate, args.downlink, firstrun, i, int(args.n), pkt_interarrival, pkt_length, 
 				pkt_length_total, pkt_list, time_previous, args.uplink)
 		i += 1
-
+		#if i == 255: i = 0
+		#print(i)
+		#print("Leftover buffer: %d\n" %len(buffer))
 		if len(pkt_interarrival) >= int(args.n): # requested number of packets generated
 			break
-		time.sleep(0.1)
+		#if i == 1500:
+		#	break
+		#time.sleep(0.1)
 	print("\nPacket generation is completed!\nGraph is being prepared, please hold on...")
-	
+	exectime = float(time.time()) - starttime
+	print("Total execution time: %d s" %exectime)
 	fig = graph_generate(datarate, args.downlink, filename_extension, pkt_interarrival, pkt_length) # generate graph
 	save_output(datarate, fig, filename_extension, pkt_interarrival, pkt_length, pkt_list, title) # save all output files
 	print("\n\nDone!")
@@ -320,8 +351,8 @@ def statistics_results(datarate, firstrun, pkt, pkt_interarrival, pkt_length, pk
 	pkt_length.append(int(len(pkt)))
 	if int(time_previous) != int(pkt.time): 
 		if not firstrun: datarate.append(float(pkt_length_total * 8 / 1000)) # multiply by 8 to convert bytes to bits, divide by 1000 to convert into kbps
+		if pkt_length_total != 0: firstrun = False # to skip the first second 
 		pkt_length_total = 0
-		firstrun = False
 	else:
 		datarate.append(float())
 	pkt_length_total += len(pkt)	
